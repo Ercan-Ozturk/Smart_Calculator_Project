45c2432061f32df80300655146cdea88a29e473f
Ercan Öztürk <152120181041@ogrenci.ogu.edu.tr>
2020-03-21 13:13:17 +0300

    Refactoring.

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -1,32 +1,28 @@
 class Smart_Calculator:
     dict = {}
     stack = []
-    """
-    def __init__(self):
-        self.dict = {}
-        self.stack = []
-    """
-    def hasNumbers(self, inputString):
+
+    def __hasNumbers(self, inputString):
         return any(char.isdigit() for char in inputString)
 
-    def haswords(self, inputString):
+    def __haswords(self, inputString):
         for i in inputString:
             if i.lower() in 'abcdefghijklmnopqrstuvwxyz':
                 return True
         return False
 
-    def eq(self, input):
+    def __eq(self, input):
         for i in input:
             if "=" in i:
                 return True
         return False
 
-    def multipleOP(self, op):
+    def __multipleOP(self, op):
         i = 0
         l = len(op) - 1
         while i < l:
 
-            if self.hasNumbers(op[i]):
+            if self.__hasNumbers(op[i]):
                 if op[i + 1] not in "-*+/":
                     return -1
             else:
@@ -41,7 +37,7 @@ class Smart_Calculator:
                             l -= 1
         return 1
 
-    def operands(self, ops):
+    def __operands(self, ops):
         tmp = ""
         op = []
         for i in ops:
@@ -92,9 +88,9 @@ class Smart_Calculator:
             op.append(tmp)
         return op
 
-    def variables(self, op):
+    def __variables(self, op):
         for i in range(len(op)):
-            if (self.haswords(self, op[i])):
+            if (self.__haswords(self, op[i])):
                 if (self.dict.get(op[i]) != None):
                     op[i] = str(self.dict.get(op[i]))
                 else:
@@ -102,15 +98,13 @@ class Smart_Calculator:
 
         return op
 
-    def paran(op):
+    def __paran(op):
         return op.count("(") == op.count(")")
 
-    def postfix(self,  postfix, op):#stack i sildim
-        # stack = []
-        # postfix = ""
+    def __postfix(self,  postfix, op):
         for i in op:
 
-            if self.hasNumbers(self, i):
+            if self.__hasNumbers(self, i):
                 postfix += " " + i
             elif len(self.stack) > 0:
                 if (self.stack[-1] == "("):
@@ -132,8 +126,6 @@ class Smart_Calculator:
                             else:
                                 break
                         self.stack.append(i)
-
-                        # postfix += " " + stack.pop()
 
                 else:
                     self.stack.append(i)
@@ -159,10 +151,10 @@ class Smart_Calculator:
         calc.pop(0)
         return calc
 
-    def calculate(self, calc):
+    def __calculate(self, calc):
         for i in calc:
 
-            if self.hasNumbers(self, i):
+            if self.__hasNumbers(self, i):
                 self.stack.append(i)
             elif i == "+":
                 one = self.stack.pop()
@@ -182,7 +174,7 @@ class Smart_Calculator:
                 self.stack.append(int(two) - int(one))
     def menu(self):
         while (True):
-
+            print("Please enter the calculation: ", end="")
             numbers = input()
             if len(numbers) > 0:
                 if numbers[0] == "/":
@@ -196,14 +188,14 @@ class Smart_Calculator:
                         print("Unknown command")
                         continue
 
-            if self.eq(self, numbers):
+            if self.__eq(self, numbers):
 
                 numbers = numbers.replace(" ", "")
                 numbers = numbers.split("=")
                 if len(numbers) > 2:
                     print("Invalid assignment")
                     continue
-                if (self.hasNumbers(self, numbers[0])):
+                if (self.__hasNumbers(self, numbers[0])):
                     print("Invalid identifier")
                     continue
                 if self.haswords(self, numbers[1]):
@@ -220,7 +212,7 @@ class Smart_Calculator:
                 continue
 
             else:
-                #stack = []
+
                 postfix_ = ""
                 numbers = numbers.replace("---", "-")
                 numbers = numbers.replace("--", "+")
@@ -228,9 +220,9 @@ class Smart_Calculator:
                 if "**" in numbers or "//" in numbers:
                     print("Invalid expression")
                     continue
-                op = self.operands(self, numbers)
-                op = self.variables(self, op)
-                print(op)
+                op = self.__operands(self, numbers)
+                op = self.__variables(self, op)
+
                 if op == -1:
                     print("Unknown variable")
                     continue
@@ -241,14 +233,14 @@ class Smart_Calculator:
                         print(op[0] + op[1])
                         continue
 
-                if self.paran(op) == False:
+                if self.__paran(op) == False:
                     print("Invalid expression")
                     continue
 
-                post = self.postfix(self, postfix_, op)
-                self.calculate(self, post)
+                post = self.__postfix(self, postfix_, op)
+                self.__calculate(self, post)
                 if (len(self.stack) != 1):
                     print("Invalid expression")
                     continue
-                print(self.stack[0])
+                print("Answer is: " + str(self.stack[0]))
 
